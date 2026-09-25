5b299dbf59f151c746d7cdc928b570cf1bfe45fd
Matthew McIntosh <matthew.n.mcintosh@outlook.com>
2026-09-25 12:38:26 -0400

    Updated models

--- server/models.py
+++ server/models.py
@@ -17,8 +17,17 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates="customer")
+
+    items = association_proxy("reviews", "item")
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+
+class CustomerSchema(Schema):
+  id = fields.Int(dump_only=True)
+  name = fields.Str(required=True)
+  reviews = fields.Nested("ReviewSchema", many=True, exclude=("customer",))
 
 
 class Item(db.Model):
@@ -28,5 +37,31 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates="item")
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class ItemSchema(Schema):
+  id = fields.Int(dump_only=True)
+  name = fields.Str(required=True)
+  price = fields.Float(required=True)
+  reviews = fields.Nested("ReviewSchema", many=True, exclude=("item",))
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates="reviews")
+    item = db.relationship('Item', back_populates="reviews")
+
+class ReviewSchema(Schema):
+  id = fields.Int(dump_only=True)
+  comment = fields.Str(required=True)
+  customer = fields.Nested("CustomerSchema", exclude=("reviews",))
+  item = fields.Nested("ItemSchema", exclude=("reviews",))
+    
